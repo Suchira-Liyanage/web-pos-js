8ee72947dbc3c9e79812c1e8153ca9049b7a7b5b
Suchira <suchiraliyanage94@gmail.com>
2020-11-15 06:58:37 +0530

    implemented add customer task

--- js/customer-ctrl.js
+++ js/customer-ctrl.js
@@ -25,6 +25,8 @@
 
 // Todo: add all global variable declaration here
 
+var btnSave = document.getElementById('btn-save');
+
 /*===============================================================================
  * Init
  *===============================================================================*/
@@ -41,8 +43,45 @@ function init(){
 
 // Todo: add all event listeners and handlers here
 
+btnSave.addEventListener('click',function (){
+    var customerID = document.getElementById('txt-id').value;
+    var customerName = document.getElementById('txt-name').value;
+    var customerAddress = document.getElementById('txt-address').value;
+    var customer = new CreateCustomer(customerID, customerName, customerAddress);
+    addTable(customer);
+});
+
 /*===============================================================================
  * Functions
  *===============================================================================*/
 
 // Todo: add all functions
+
+function CreateCustomer(id, name, address){
+    this.id = id;
+    this.name = name;
+    this.address = address;
+};
+
+function addTable(customer){
+    var tableBody = document.getElementById('tableBody');
+
+    var image = document.createElement('img');
+    image.setAttribute('src','./img/trash.png');
+    image.className = 'deleteIcon';
+    image.style.width = '32px';
+    image.style.height = '32px';
+
+    var newRow = tableBody.insertRow(-1);
+    var cusID = newRow.insertCell(0);
+    var cusName = newRow.insertCell(1);
+    var cusAddress = newRow.insertCell(2);
+    var cusDelete = newRow.insertCell(3);
+    cusID.innerText = customer.id;
+    cusID.style.textAlign = 'center';
+    cusName.innerText = customer.name;
+    cusAddress.innerText = customer.address;
+    cusDelete.append(image);
+    cusDelete.style.textAlign = 'center';
+
+}
