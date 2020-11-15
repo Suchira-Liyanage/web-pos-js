c9372da2fc0efcaaaaba8684fb99e692ac984ae3
Suchira <suchiraliyanage94@gmail.com>
2020-11-15 08:22:48 +0530

    Implement popup messages

--- js/customer-ctrl.js
+++ js/customer-ctrl.js
@@ -62,6 +62,28 @@ customerTable.addEventListener('mouseover',function (event){
     }
 });
 
+customerTable.addEventListener('click',function (event){
+    if (event.target.className === 'deleteIcon'){
+        Swal.fire({
+            title: 'Are you sure whether you want to delete this customer?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
+    }
+});
+
 setInterval(function (){
     if (customerTable.rows.length > 2){
         customerTable.deleteTFoot();
